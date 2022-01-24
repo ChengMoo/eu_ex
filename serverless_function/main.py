07b7bda27f038342a8ddc81adfe3c922572b665d
ChengMoo <67187215+ChengMoo@users.noreply.github.com>
2022-01-24 22:37:06 +0800

    Update main.py

--- serverless_function/main.py
+++ serverless_function/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 #
-# SPDX-FileCopyrightText: (c) 2020-2021 CokeMine & Its repository contributors
-# SPDX-FileCopyrightText: (c) 2021 A beam of light
+# SPDX-FileCopyrightText: (c) 2021-2022 A beam of light
+# SPDX-FileCopyrightText: (c) 2020-2021 CokeMine & its repository contributors
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
@@ -12,11 +12,14 @@ euserv auto-renew script
 
 ChangeLog
 
-v2021.09.30
-- Captcha automatic recognition using TrueCaptcha API
-- Email notification
-- Add login failure retry mechanism
-- reformat log info
+v2021.12.15
+- Implemented a simple localization system, log output localization
+- Reformat code via black
+
+v2021.11.26
+- Handle TrueCaptcha service exception
+- Adjust TrueCaptcha constraint parameters for high availability.
+  Plus, the CAPTCHA of EUserv is currently case-insensitive, so the above adjustment works.
 
 v2021.11.06
 - Receive renew PIN(6-digits) using mailparser parsed data download url
@@ -24,14 +27,11 @@ v2021.11.06
   -> parsing PIN via mailparser -> get PIN from mailparser
 - Update kc2_security_password_get_token request
 
-v2021.11.26
-- Handle TrueCaptcha service exception
-- Adjust TrueCaptcha constraint parameters for high availability.
-  Plus, the CAPTCHA of EUserv is currently case-insensitive, so the above adjustment works.
-
-v2021.12.15
-- Implemented a simple localization system, log output localization
-- Reformat code via black
+v2021.09.30
+- Captcha automatic recognition using TrueCaptcha API
+- Email notification
+- Add login failure retry mechanism
+- reformat log info
 
 """
 
@@ -110,7 +110,7 @@ CHECK_CAPTCHA_SOLVER_USAGE = True
 
 user_agent = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/96.0.4664.110 Safari/537.36"
+    "Chrome/97.0.4692.71 Safari/537.36"
 )
 
 # Blank
@@ -127,7 +127,7 @@ chs_locale = {
     "Login retried the @@@ time": "登录重试第 @@@ 次",
     "You are using the demo apikey": "你正在使用演示版 apikey",
     "There is no guarantee that demo apikey will work in the future": "无法保证演示版 apikey 在将来也能使用",
-    "You are using your own apikey": " 你正在使用自己的 apikey",
+    "You are using your own apikey": "你正在使用自己的 apikey",
     "Service Exception": "服务异常",
     "Returned JSON": "返回的 JSON",
     "Failed to find parsed results": "找不到解析结果",
@@ -172,7 +172,7 @@ cht_locale = {
     "Login retried the @@@ time": "登錄重試第 @@@ 次",
     "You are using the demo apikey": "你正在使用演示版 apikey",
     "There is no guarantee that demo apikey will work in the future": "無法保證演示版 apikey 在將來也能使用",
-    "You are using your own apikey": " 你正在使用你自己的 apikey",
+    "You are using your own apikey": "你正在使用你自己的 apikey",
     "Service Exception": "服務異常",
     "Returned JSON": "返回的 JSON",
     "Failed to find parsed results": "找不到解析結果",
